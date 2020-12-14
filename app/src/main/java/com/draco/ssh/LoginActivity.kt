3d786f75c200648ba77e26b215ca309cd178ceb9
Tyler Nijmeh <tylernij@gmail.com>
2020-12-14 10:47:29 -0800

    Cleanup and improvements
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- app/src/main/java/com/draco/ssh/LoginActivity.kt
+++ app/src/main/java/com/draco/ssh/LoginActivity.kt
@@ -1,11 +1,56 @@
 package com.draco.ssh
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var address: TextInputEditText
+    private lateinit var port: TextInputEditText
+    private lateinit var username: TextInputEditText
+    private lateinit var password: TextInputEditText
+    private lateinit var start: ExtendedFloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        address = findViewById(R.id.address)
+        port = findViewById(R.id.port)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        start = findViewById(R.id.start)
+
+        getPreferences(Context.MODE_PRIVATE).apply {
+            address.setText(getString("address", "192.168.0.1"))
+            port.setText(getString("port", "22"))
+            username.setText(getString("username", "root"))
+            password.setText(getString("password", ""))
+        }
+
+        start.setOnClickListener {
+            val intent = Intent(this, ShellActivity::class.java)
+                .putExtra("address", address.text.toString())
+                .putExtra("port", port.text.toString())
+                .putExtra("username", username.text.toString())
+                .putExtra("password", password.text.toString())
+            startActivity(intent)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        getPreferences(Context.MODE_PRIVATE).apply {
+            with (edit()) {
+                putString("address", address.text.toString())
+                putString("port", port.text.toString())
+                putString("username", username.text.toString())
+                putString("password", password.text.toString())
+                apply()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/draco/ssh/ShellActivity.kt
@@ -0,0 +1,180 @@
+package com.draco.ssh
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.ScrollView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
+import com.jcraft.jsch.ChannelExec
+import com.jcraft.jsch.JSch
+import com.jcraft.jsch.JSchException
+import com.jcraft.jsch.Session
+import java.io.File
+
+class ShellActivity : AppCompatActivity() {
+    companion object {
+        const val MAX_CONNECTION_TIMEOUT = 60 * 1000
+        const val MAX_OUTPUT_BUFFER_SIZE = 1024 * 4
+        const val OUTPUT_BUFFER_DELAY_MS = 100L
+    }
+
+    private lateinit var progress: ProgressBar
+    private lateinit var command: TextInputEditText
+    private lateinit var output: MaterialTextView
+    private lateinit var outputScrollView: ScrollView
+
+    private lateinit var address: String
+    private var port = 22
+    private lateinit var username: String
+    private lateinit var password: String
+
+    private lateinit var jSch: JSch
+    private lateinit var session: Session
+
+    private lateinit var outputBuffer: File
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shell)
+
+        progress = findViewById(R.id.progress)
+        command = findViewById(R.id.command)
+        output = findViewById(R.id.output)
+        outputScrollView = findViewById(R.id.output_scrollview)
+
+        address = intent.getStringExtra("address")!!
+        try {
+            port = Integer.parseInt(intent.getStringExtra("port")!!)
+        } catch (_: Exception) { }
+        username = intent.getStringExtra("username")!!
+        password = intent.getStringExtra("password")!!
+
+        jSch = JSch()
+        session = jSch.getSession(username, address, port)
+        session.setPassword(password)
+        session.setConfig("StrictHostKeyChecking", "no")
+
+        connect()
+
+        /* Store the buffer locally to avoid an OOM error */
+        outputBuffer = File.createTempFile("buffer", "txt").apply {
+            deleteOnExit()
+        }
+
+        command.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
+                /* Send command and disable input until completion */
+                output.text = null
+                sendCommand(command.text.toString())
+                command.isEnabled = false
+
+                return@setOnKeyListener true
+            }
+
+            return@setOnKeyListener false
+        }
+    }
+
+    private fun sendCommand(thisCommand: String) {
+        Thread {
+            /* Open the exec channel */
+            (session.openChannel("exec") as ChannelExec).apply {
+                /* Initialize stream */
+                outputStream = outputBuffer.outputStream()
+                setErrStream(outputBuffer.outputStream())
+                setCommand(thisCommand)
+
+                /* Start the progress bar */
+                runOnUiThread {
+                    progress.visibility = View.VISIBLE
+                }
+
+                /* Execute */
+                connect(MAX_CONNECTION_TIMEOUT)
+
+                /* Until finished, update the output */
+                while (!isClosed) {
+                    val out = readEndOfFile(outputBuffer)
+                    runOnUiThread {
+                        output.text = out
+                    }
+                    outputScrollView.post {
+                        outputScrollView.fullScroll(ScrollView.FOCUS_DOWN)
+                    }
+                    Thread.sleep(OUTPUT_BUFFER_DELAY_MS)
+                }
+
+                /* Finish */
+                disconnect()
+            }
+
+            /* Hide the progress and update the final output */
+            val out = readEndOfFile(outputBuffer)
+            runOnUiThread {
+                progress.visibility = View.INVISIBLE
+                runOnUiThread {
+                    output.text = out
+                }
+                outputScrollView.post {
+                    outputScrollView.fullScroll(ScrollView.FOCUS_DOWN)
+                }
+                command.isEnabled = true
+            }
+        }.start()
+    }
+
+    private fun readEndOfFile(file: File): String {
+        val out = ByteArray(MAX_OUTPUT_BUFFER_SIZE)
+        file.inputStream().use {
+            val size = it.channel.size()
+
+            if (size <= out.size)
+                return String(it.readBytes())
+
+            val newPos = (it.channel.size() - out.size)
+            it.channel.position(newPos)
+            it.read(out)
+        }
+
+        return String(out)
+    }
+
+    private fun connect() {
+        Thread {
+            try {
+                session.connect(MAX_CONNECTION_TIMEOUT)
+                
+                /* Unlock UI */
+                runOnUiThread {
+                    progress.visibility = View.INVISIBLE
+                    command.isEnabled = true
+                }
+            } catch (e: JSchException) {
+                e.printStackTrace()
+
+                runOnUiThread {
+                    error(e.message!!)
+                }
+            }
+        }.start()
+    }
+
+    private fun error(exceptionMessage: String) {
+        MaterialAlertDialogBuilder(this).apply {
+            setTitle(R.string.error_title)
+            setPositiveButton(R.string.error_disconnect) { _, _ -> finish() }
+            setMessage(exceptionMessage)
+            setCancelable(false)
+            show()
+        }
+    }
+
+    override fun onDestroy() {
+        session.disconnect()
+        super.onDestroy()
+    }
+}
