a606cdec35c8414ecae339a7345d0fef4d893621
Tyler Nijmeh <tylernij@gmail.com>
2020-12-14 06:52:34 -0800

    Initial commit
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ app/src/main/java/com/draco/ssh/LoginActivity.kt
@@ -0,0 +1,10 @@
+package com.draco.ssh
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
